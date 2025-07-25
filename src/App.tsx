d84bc61a23c53a75f546210d0700fe290af43fef
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2025-07-25 08:08:45 -0700

    Added the Eventhandler

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import { ListGroup } from "react-bootstrap";
 import MikeListGroup from "./components/MikeListGroup";
 
-// Next time pick up at conditional randering in the component section of the codewithMosh website.
+// Next time pick up at state management in the component section of the codewithMosh website. Go back to the MikeListGroup.
 
 function App() {
   const [BootStrapAlertVisable, setBootstrapAlertVisable]=useState(false)
--- src/components/MikeListGroup.tsx
+++ src/components/MikeListGroup.tsx
@@ -1,12 +1,19 @@
 import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { MouseEvent } from "react";
 
 function mikeListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  //Event handler function. the : MounseEvent is to specify what type of event is this.
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <ListGroup>
-      {items.map((item) => (
-        <ListGroupItem key ={item}>{item}</ListGroupItem>
+      {items.length === 0 && <p> No item found</p>}
+      {items.map((item, index) => (
+        <ListGroupItem key={item} onClick={handleClick}>
+          {item}
+        </ListGroupItem>
       ))}
     </ListGroup>
   );
